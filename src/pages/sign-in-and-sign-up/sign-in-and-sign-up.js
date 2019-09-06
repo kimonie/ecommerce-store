e9781c7df13afebe923fff745e710d0941585b54
Seuun <seunkikiowo@gmail.com>
2019-09-06 21:18:20 +0100

    added sign up component

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import SignIn from '../../components/sign-in/sign-in'
+import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
-import './sign-in-and-sign-up.scss'
-
-
+import './sign-in-and-sign-up.scss';
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'><SignIn /></div>
-    
-
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+        <SignUp />
+    </div>
 );
 
 export default SignInAndSignUpPage;
